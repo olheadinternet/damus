eb02170ac7ca0d9ac7f9dc316ffb8fb592582f0b
Zach Hendel <56426913+olheadinternet@users.noreply.github.com>
2023-07-27 23:36:39 -0700

    add header orange gradient adjustment

--- damus/Components/Gradients/PinkGradient.swift
+++ damus/Components/Gradients/PinkGradient.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-fileprivate let damus_grad_c1 = hex_col(r: 0xe7, g: 0xc4, b: 0x83)
-fileprivate let damus_grad_c2 = hex_col(r: 0xf5, g: 0x95, b: 0x21)
+fileprivate let damus_grad_c1 = hex_col(r: 0xf5, g: 0x95, b: 0x21)
+fileprivate let damus_grad_c2 = hex_col(r: 0xf2, g: 0xa3, b: 0x3c)
 fileprivate let pink_grad = [damus_grad_c1, damus_grad_c2]
 
 let PinkGradient = LinearGradient(colors: pink_grad, startPoint: .topTrailing, endPoint: .bottom)
